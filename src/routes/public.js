15a18fc1eab824e8567fefb629cdf5bca598a417
MichelHoffmann <mic.hoff.jr@gmail.com>
2025-02-10 07:43:15 -0500

    anything

--- src/routes/public.js
+++ src/routes/public.js
@@ -1,9 +1,13 @@
 import express from 'express'
+const { PrismaClient } = require("@prisma/client");
 
+const prisma = new PrismaClient();
 const router = express.Router()
 
-router.post('/cadastro', (req, res) => {
+router.post('/cadastro', async (req, res) => {
     const user = req.body
+
+    await prisma.user.create 
 
     res.status(201).json(user)
 })
